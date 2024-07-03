3da52178d8886fcc3f48ef9dbc4ba780d33ac988
Sumit-pd <pdsumit07@gmail.com>
2024-07-03 13:54:20 +0530

    Initial Commit

---
+++ TLMS-admin/TLMS_adminApp.swift
@@ -0,0 +1,17 @@
+//
+//  TLMS_adminApp.swift
+//  TLMS-admin
+//
+//  Created by Sumit Prasad on 03/07/24.
+//
+
+import SwiftUI
+
+@main
+struct TLMS_adminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
