4afcc964cb7f2fd9ac4b51392448e56ba31aaf7a
Paras Singhal <parassinghal2310@gmail.com>
2024-07-15 01:29:47 +0530

    Connected Admin front-end to Backend

---
+++ TLMS-admin/Admin App/EducatorListView.swift
@@ -0,0 +1,18 @@
+//
+//  EducatorListView.swift
+//  TLMS-admin
+//
+//  Created by Abcom on 15/07/24.
+//
+
+import SwiftUI
+
+struct EducatorListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EducatorListView()
+}
---
+++ TLMS-admin/Admin App/NotificationView.swift
@@ -0,0 +1,18 @@
+//
+//  NotificationView.swift
+//  TLMS-admin
+//
+//  Created by Abcom on 15/07/24.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NotificationView()
+}
